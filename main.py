58a914ec948356e4aaea3729848684aeb5f312fe
Yassien Tawfik <yassien.m.m.tawfik@gmail.com>
2025-08-02 15:52:52 +0300

    Initial set-ups

---
+++ main.py
@@ -0,0 +1,10 @@
+import pandas as pd
+from src.__01_data_setup import download_dataset
+
+
+def main():
+    print(f"Creating Needed Directories...")
+
+
+if __name__ == "__main__":
+    main()
---
+++ src/__01_data_setup.py
@@ -0,0 +1,10 @@
+import kagglehub
+
+
+def download_dataset():
+    pass
+
+# Download latest version
+# path = kagglehub.dataset_download("aslanahmedov/walmart-sales-forecast")
+#
+# print("Path to dataset files:", path)
