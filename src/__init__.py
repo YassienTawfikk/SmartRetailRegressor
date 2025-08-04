07a3105a4ac45ff80c9c702e95231e688b4413a3
Yassien Tawfik <yassien.m.m.tawfik@gmail.com>
2025-08-05 01:34:08 +0300

    Update directory creation logic to include `model_dir` paths

--- src/__init__.py
+++ src/__init__.py
@@ -1,5 +1,5 @@
-from src.__00__paths import data_dir_list, output_dir_list
+from src.__00__paths import data_dir_list, model_dir
 
 # Iterate all over the two lists to create directories
-for path in data_dir_list + output_dir_list:
+for path in data_dir_list + [model_dir]:
     path.mkdir(parents=True, exist_ok=True)
